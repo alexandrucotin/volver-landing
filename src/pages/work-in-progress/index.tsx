40fcc8ec0878abd68abacc0d222857714b725b12
Alexandru Coti <alexandrucoti@Alexandrus-MacBook-Pro.local>
2024-05-23 19:26:09 +0200

    Fix work in progress page

--- src/pages/work-in-progress/index.tsx
+++ src/pages/work-in-progress/index.tsx
@@ -1,26 +1,30 @@
 import React from "react";
 import volverLogo from "../../assets/logo.png";
+import Footer from "../../components/molecules/footer";
 
 const WorkInProgressPage: React.FC = () => {
   return (
-    <div className="landing-page">
-      <div className="landing-page-logo-container">
-        <img src={volverLogo} alt="Volver studio logo" />
-      </div>
-      <div className="landing-page-content">
-        <div className="landing-page-content-work-in-progress">
-          Website work in progress
+    <div className="work-in-progress">
+      <div className="container">
+        <div className="work-in-progress-logo-container">
+          <img src={volverLogo} alt="Volver studio logo" />
         </div>
-        <h1 className="landing-page-content-slogan">
-          Where creativity comes <br /> full circle
-        </h1>
-        <a
-          href="mailto:info@volver.studio?subject=Richiesta-Informazioni"
-          className="landing-page-content-cta"
-        >
-          Contact Us
-        </a>
+        <div className="work-in-progress-content">
+          <div className="work-in-progress-content-work-in-progress">
+            Website work in progress
+          </div>
+          <h1 className="work-in-progress-content-slogan">
+            Where creativity comes <br /> full circle
+          </h1>
+          <a
+            href="mailto:info@volver.studio?subject=Richiesta-Informazioni"
+            className="work-in-progress-content-cta"
+          >
+            Contact Us
+          </a>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 };
