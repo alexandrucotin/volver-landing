51a3f85db54f916f4e4c4d5c5645be3b545bc065
Alexandru Coti <alexandrucoti@Alexandrus-MacBook-Pro.local>
2024-06-26 21:45:25 +0200

    Fix scrolling

--- src/core/layout/index.tsx
+++ src/core/layout/index.tsx
@@ -1,8 +1,15 @@
-import { ScrollRestoration, Outlet } from "react-router-dom";
+import { ScrollRestoration, Outlet, useLocation } from "react-router-dom";
 import ProtectedRoute from "../protected-route";
 import Menu from "../../components/organisms/navbar";
+import { useEffect } from "react";
 
 const AppLayout: React.FC = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div
       className=""
