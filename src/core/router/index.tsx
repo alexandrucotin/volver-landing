21e7ce0724f0c364f597585129b10b6f169966c0
Alexandru Coti <alexandrucoti@Alexandrus-MacBook-Pro.local>
2024-06-26 21:26:27 +0200

    Fix loader in work in progress page

--- src/core/router/index.tsx
+++ src/core/router/index.tsx
@@ -43,7 +43,11 @@ const Router: React.FC = () => {
   ]);
   return (
     <>
-      <AnimatePresence>{isLoading && <GlobalLoader />}</AnimatePresence>
+      <AnimatePresence>
+        {isLoading && window.location.href.split("/")[3] !== "" && (
+          <GlobalLoader />
+        )}
+      </AnimatePresence>
       <RouterProvider router={router} />
     </>
   );
